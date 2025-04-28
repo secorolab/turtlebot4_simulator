15f556b30b86842ff00c31afd25ad204f65f770d
KishanSawant <kishansawant96@gmail.com>
2025-04-28 16:01:12 +0200

    allow setting gz_sim_resource_path env variable as launch arg

--- turtlebot4_gz_bringup/launch/sim.launch.py
+++ turtlebot4_gz_bringup/launch/sim.launch.py
@@ -19,12 +19,15 @@ import os
 from pathlib import Path
 
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import ExecuteProcess
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable, OpaqueFunction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from pathlib import Path
+
 from launch_ros.actions import Node
 
 
@@ -37,27 +40,30 @@ ARGUMENTS = [
     DeclareLaunchArgument('model', default_value='lite',
                           choices=['standard', 'lite'],
                           description='Turtlebot4 Model'),
+    DeclareLaunchArgument('gz_resource_path', default_value='',
+                          description='Gazebo resource path'),
+    DeclareLaunchArgument('gz_model_path', default_value='',
+                            description='Gazebo model path'),
 ]
 
 
-def generate_launch_description():
-
+def set_gz_resource_path_env_variable(context):
+    """
+    Set the GZ_SIM_RESOURCE_PATH environment variable based on the
+    compile time argument value.
+    """
     # Directories
     pkg_turtlebot4_gz_bringup = get_package_share_directory(
         'turtlebot4_gz_bringup')
-    pkg_turtlebot4_gz_gui_plugins = get_package_share_directory(
-        'turtlebot4_gz_gui_plugins')
     pkg_turtlebot4_description = get_package_share_directory(
         'turtlebot4_description')
     pkg_irobot_create_description = get_package_share_directory(
         'irobot_create_description')
     pkg_irobot_create_gz_bringup = get_package_share_directory(
         'irobot_create_gz_bringup')
-    pkg_irobot_create_gz_plugins = get_package_share_directory(
-        'irobot_create_gz_plugins')
-    pkg_ros_gz_sim = get_package_share_directory(
-        'ros_gz_sim')
-
+    
+    gz_resource_path_env_variable = LaunchConfiguration('gz_resource_path').perform(context)
+            
     # Set Gazebo resource path
     gz_resource_path = SetEnvironmentVariable(
         name='GZ_SIM_RESOURCE_PATH',
@@ -65,10 +71,28 @@ def generate_launch_description():
             os.path.join(pkg_turtlebot4_gz_bringup, 'worlds'),
             os.path.join(pkg_irobot_create_gz_bringup, 'worlds'),
             str(Path(pkg_turtlebot4_description).parent.resolve()),
-            str(Path(pkg_irobot_create_description).parent.resolve())
+            str(Path(pkg_irobot_create_description).parent.resolve()),
+            gz_resource_path_env_variable,
         ])
     )
+    
+    # Get the compile time argument value from the context
+    return [gz_resource_path]
 
+def generate_launch_description():
+
+    # Directories
+    pkg_turtlebot4_gz_gui_plugins = get_package_share_directory(
+        'turtlebot4_gz_gui_plugins')
+    pkg_irobot_create_gz_plugins = get_package_share_directory(
+        'irobot_create_gz_plugins')
+    pkg_ros_gz_sim = get_package_share_directory(
+        'ros_gz_sim')
+
+    verify_gz_env = ExecuteProcess(
+        cmd=['sh', '-c', 'echo GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH'],
+        output='screen'
+    )
     gz_gui_plugin_path = SetEnvironmentVariable(
         name='GZ_GUI_PLUGIN_PATH',
         value=':'.join([
@@ -81,7 +105,7 @@ def generate_launch_description():
     gz_sim_launch = PathJoinSubstitution(
         [pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'])
 
-    # Gazebo harmonic
+    # Gazebo harmonic (commenting gui for better performance)
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([gz_sim_launch]),
         launch_arguments=[
@@ -111,8 +135,9 @@ def generate_launch_description():
 
     # Create launch description and add actions
     ld = LaunchDescription(ARGUMENTS)
-    ld.add_action(gz_resource_path)
-    # ld.add_action(gz_gui_plugin_path)
+    ld.add_action(OpaqueFunction(function=set_gz_resource_path_env_variable))
+    ld.add_action(verify_gz_env)
+    # ld.add_action(gz_gui_plugin_path) # commenting gui for better performance
     ld.add_action(gazebo)
     ld.add_action(clock_bridge)
     return ld
